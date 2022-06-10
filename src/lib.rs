e159ab8dceceec22a929324a6530a3e3fcf650fd
Dorian Vuolo <dorian.vuolo@kwote.fr>
2022-06-10 15:07:40 +0200

    Added cancel method

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod task;
 
 pub use registry::TaskDescriptor;
 pub use task::{Routine, Task};
+use anyhow::Result;
 
 pub async fn list_all() -> Vec<TaskDescriptor> {
     registry::list_all().await
@@ -10,4 +11,8 @@ pub async fn list_all() -> Vec<TaskDescriptor> {
 
 pub async fn count() -> usize {
     registry::count().await
+}
+
+pub async fn cancel(id: u64) -> Result<()> {
+    registry::cancel(id).await
 }
